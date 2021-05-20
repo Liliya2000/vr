d311ab49ada142effabc25032d2feef90633c017
liliya <liliya.p.2000@gmail.com>
2021-05-20 13:32:34 +0300

    20.05.21

--- Assets/rotate.cs
+++ Assets/rotate.cs
@@ -8,4 +8,12 @@ public class rotate : MonoBehaviour
     {
         gameObject.transform.Rotate(new Vector3(0, 10, 0));
     }
+
+    //if (timer <= 1)
+    //    {
+    //        // Time.deltaTime*100 will make sure we are moving at a constant speed of 100 per second
+    //        transform.Rotate(0f, 0f, Time.deltaTime* 100);
+    //        // Increment the timer so we know when to stop
+    //        timer += Time.deltaTime;
+    //    }
 }

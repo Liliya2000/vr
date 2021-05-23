41148935a75461c8fe457a8436f8e0badced8516
liliya <liliya.p.2000@gmail.com>
2021-05-23 23:57:33 +0300

    надеюсь не сломала ничего

---
+++ Assets/MyLocomotionController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class MyLocomotionController : MonoBehaviour
+{
+    public XRController leftTeleportRay;
+
+    public InputHelpers.Button teleportActivationButton;
+    public float activationTherechold = 0.1f;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+       if (leftTeleportRay)
+        {
+            leftTeleportRay.gameObject.SetActive(CheckIfActivated(leftTeleportRay));
+        }
+    }
+
+    public bool CheckIfActivated(XRController controller)
+    {
+        InputHelpers.IsPressed(controller.inputDevice, teleportActivationButton, out bool isActivated, activationTherechold);
+        return isActivated;
+    }
+}
